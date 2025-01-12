56a5702e961d1117fede822e641f164c88a23cdd
Jean-Max Marie-Sainte <jeanmaxmariesainte@gmail.com>
2025-01-12 16:35:28 +0100

    Initialize project

---
+++ src/test/java/com/wizen/it/pubsubsubscriber/PubsubSubscriberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wizen.it.pubsubsubscriber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PubsubSubscriberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
